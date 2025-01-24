881642228ce09694bf9be6ce8adaa088e02b2ad1
Yassin Deli <yassindeli24@gmail.com>
2025-01-25 00:26:02 +0100

    feat(Sign in with Google) - Add Google sign in button - Add Google sign in functionality -add react-google-login package

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './app/login/login-page';
 import SignupPage from './app/login/signup-page';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/" element={<LoginPage />} /> {/* Default route */}
-      </Routes>
-    </Router>
+    <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/" element={<LoginPage />} /> {/* Default route */}
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
